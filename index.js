8ed54857051d9eaba5269334b0d27cb599c13819
Gustavo Lima <gustavolimamendes2@gmail.com>
2024-07-02 21:42:13 -0300

    Informações no corpo da requisição com Axios

--- index.js
+++ index.js
@@ -3,18 +3,26 @@ const app = express();
 
 app.use(express.json());
 
-app.get('/carros', async (req, res) => {
-    res.json([
-        {
-            modelo: "S10",
-            marca:"Chevrolet"
-        },
-        {
-            modelo: "Cruze",
-            marca:"Chevrolet"
-        }
+const carros = [
+    {
+        modelo: "S10",
+        marca:"Chevrolet"
+    },
+    {
+        modelo: "Cruze",
+        marca:"Chevrolet"
+    }
+]
 
-    ])
+app.get('/carros', async (req, res) => {
+    res.json(carros)
+});
+
+app.post('/carros', async (req, res) => {
+    const {modelo, marca} = req.body;
+
+    carros.push({modelo, marca});
+    res.json(carros)
 });
 
 app.listen(3001);

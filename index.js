a21c5bc21a561f735a7bea09797a4b8482425347
Gustavo Lima <gustavolimamendes2@gmail.com>
2024-07-02 21:35:28 -0300

    Integração entre duas API's com Axios

--- index.js
+++ index.js
@@ -2,4 +2,19 @@ const express = require('express');
 const app = express();
 
 app.use(express.json());
+
+app.get('/carros', async (req, res) => {
+    res.json([
+        {
+            modelo: "S10",
+            marca:"Chevrolet"
+        },
+        {
+            modelo: "Cruze",
+            marca:"Chevrolet"
+        }
+
+    ])
+});
+
 app.listen(3001);
